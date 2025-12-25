b5292d01c96f02cc397b4207646aaebc7d950a48
sxndev <sxnsilva@gmail.com>
2025-12-25 15:46:20 -0300

    site atualizado para um desgin mais moderno, sem media queries criadas, responsividade pendente

--- src/script/script.js
+++ src/script/script.js
@@ -4,9 +4,8 @@ const myObserver = new IntersectionObserver( (elements) =>{
     elements.forEach((element) => {
         if(element.isIntersecting){
             element.target.classList.add('show')
-        } else{
-            element.target.classList.remove('show')
-        }
+        } 
     })
 } )  
         hidden.forEach( (element) => myObserver.observe(element))
+
