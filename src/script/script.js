047a9e0098ac2f838dd90d2cc57c25eab5e39f5d
sxndev <sxnsilva@gmail.com>
2025-12-29 22:31:21 -0300

    alteração no caminho das imagens

--- src/script/script.js
+++ src/script/script.js
@@ -1,11 +1,27 @@
 const hidden = document.querySelectorAll('.hidden');
+const aboutText = document.getElementById('about-text')
+let index = 0
 
-const myObserver = new IntersectionObserver( (elements) =>{
+function typeEffet(){
+    if(index < aboutText.textContent.length){
+        aboutText.textContent += aboutText.charAt(index)
+        index++
+        setTimeout(typeEffet, 50)
+    }
+}
+const MyObeserverText = new IntersectionObserver((elements) => {
+    if(elements.isIntersecting){
+        typeEffet()
+    }
+}) 
+    MyObeserverText.observe(aboutText)
+
+const myObserverHidden = new IntersectionObserver( (elements) =>{
     elements.forEach((element) => {
         if(element.isIntersecting){
             element.target.classList.add('show')
         } 
     })
 } )  
-        hidden.forEach( (element) => myObserver.observe(element))
+    hidden.forEach( (element) => myObserverHidden.observe(element))
 
