f374f267ba44379cb6d0812f4a1eb323f5141cbf
sxndev <sxnsilva@gmail.com>
2025-12-30 03:48:47 -0300

    responsivo feito e problema das imagens 100% resolvido

--- src/script/script.js
+++ src/script/script.js
@@ -1,20 +1,4 @@
 const hidden = document.querySelectorAll('.hidden');
-const aboutText = document.getElementById('about-text')
-let index = 0
-
-function typeEffet(){
-    if(index < aboutText.textContent.length){
-        aboutText.textContent += aboutText.charAt(index)
-        index++
-        setTimeout(typeEffet, 50)
-    }
-}
-const MyObeserverText = new IntersectionObserver((elements) => {
-    if(elements.isIntersecting){
-        typeEffet()
-    }
-}) 
-    MyObeserverText.observe(aboutText)
 
 const myObserverHidden = new IntersectionObserver( (elements) =>{
     elements.forEach((element) => {
@@ -25,3 +9,13 @@ const myObserverHidden = new IntersectionObserver( (elements) =>{
 } )  
     hidden.forEach( (element) => myObserverHidden.observe(element))
 
+const aboutText = document.getElementById('about-text')
+const aboutImage = document.getElementById('about-image')
+
+const myObserverAbout = new IntersectionObserver( (elements) => {
+    elements.forEach((element) => {
+        if(element.isIntersecting){
+            element.target.classList.add('show-about')
+        }
+    })
+})
