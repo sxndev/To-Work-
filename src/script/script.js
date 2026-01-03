27333d9d71803c274d58d5158b43a7d10beb1627
sxndev <sxnsilva@gmail.com>
2026-01-03 19:23:32 -0300

    backgrounds animados adicionados e correção no atributo alt de algumas imagens

--- src/script/script.js
+++ src/script/script.js
@@ -8,14 +8,3 @@ const myObserverHidden = new IntersectionObserver( (elements) =>{
     })
 } )  
     hidden.forEach( (element) => myObserverHidden.observe(element))
-
-const aboutText = document.getElementById('about-text')
-const aboutImage = document.getElementById('about-image')
-
-const myObserverAbout = new IntersectionObserver( (elements) => {
-    elements.forEach((element) => {
-        if(element.isIntersecting){
-            element.target.classList.add('show-about')
-        }
-    })
-})
